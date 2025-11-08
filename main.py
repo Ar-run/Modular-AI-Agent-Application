17252ff22c08dced64a6df19fe044f8e54ec8759
Arunendra <singharun10235@gmail.com>
2025-11-08 17:50:54 +0530

    Initialied the project and installed dependencies

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI(name="multi-model-application")
+
+@app.get("/health")
+
+async def health_check():
+    return {"status" : "healthy"}
+
+def main():
+    print("Hello from multi-model-application!")
+
+
+if __name__ == "__main__":
+    main()
